663b1e476a708c74892e3c0096c09ce9967a5fe8
Egor Grishin <egor-grishin@bk.ru>
2024-05-19 20:06:35 +0300

    test settings

--- app/Core/Parents/Test.php
+++ app/Core/Parents/Test.php
@@ -5,6 +5,7 @@ namespace Core\Parents;
 use Core\Classes\Auth\Jwt;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Support\Facades\Event;
 
 abstract class Test extends TestCase
 {
@@ -16,5 +17,15 @@ abstract class Test extends TestCase
     {
         $this->jwt = new Jwt();
         parent::__construct($name);
+    }
+
+    /**
+     * Предотвращает выполнение слушателей событий за исключением событий модели
+     */
+    protected function fakeEventWithModel(): void
+    {
+        $dispatcher = Event::getFacadeRoot();
+        Event::fake();
+        Model::setEventDispatcher($dispatcher);
     }
 }
