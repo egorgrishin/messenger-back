d4ea465c38746c3f17d74720898cd42a9be6d754
Egor Grishin <egor-grishin@bk.ru>
2024-05-19 20:06:47 +0300

    validation rule

---
+++ app/Modules/Chat/Rules/ValueInArray.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Chat\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Translation\PotentiallyTranslatedString;
+
+class ValueInArray implements ValidationRule
+{
+    public function __construct(
+        private readonly mixed $value,
+    ) {}
+
+    /**
+     * Run the validation rule.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @param Closure(string): PotentiallyTranslatedString $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!in_array($this->value, $value)) {
+            $fail("Element $this->value must be represented in the array.");
+        }
+    }
+}
